54cc7abba7f7a3fa52a496e032b55155467eeb60
glitterb0mb <srd.dullflame@gmail.com>
2022-05-14 22:38:05 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>weather lol</h1>
-      <footer>
-        <a
-          href="https://github.com/glitterb0mb/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-Source Code by Sarah Davis
-        </a>
-      </footer>
+      <div className="container">
+        <h1>weather lol</h1>
+        <footer>
+          <a
+            href="https://github.com/glitterb0mb/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-Source Code by Sarah Davis
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
