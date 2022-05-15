d51c3fa1a43efaad4ac3ad3f89b2125ac9b8e3f8
glitterb0mb <srd.dullflame@gmail.com>
2022-05-14 23:29:47 -0400

    structuring layout

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,33 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather"> lol weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <input type="search">Search Location</input>
+      </form>
+      <h1>Atlanta, GA</h1>
+      <ul>
+        <li>Saturday 10:45PM</li>
+        <li>Clear</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="partly cloudy"
+          />
+          71°F
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 74%</li>
+            <li>Wind Speed: 1MPH</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
