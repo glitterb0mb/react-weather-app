2bc06175f5d6631f4c746a432296a3527c93ea2f
glitterb0mb <srd.dullflame@gmail.com>
2022-05-15 20:13:23 -0400

    icons

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       city: response.data.name,
       description: response.data.weather.description,
       date: new Date(response.data.dt * 1000),
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
     });
 
     function search() {
